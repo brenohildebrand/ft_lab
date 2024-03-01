4a073633348830a3de9a0f30a4a41bf8187fbbb8
Breno Hildebrand <hello@brenohildebrand.com>
2024-03-01 19:04:09 -0300

    try

---
+++ build/debug/dependencies/lab.d
@@ -0,0 +1,17 @@
+build/debug/objects/lab.o: source/lab.c \
+ /home/bhildebr/github/ft_framework/source/types/memtree/memtree.h \
+ /home/bhildebr/github/ft_framework/source/types/any/any.h \
+ /home/bhildebr/github/ft_framework/source/functions/allocate/allocate.h \
+ /home/bhildebr/github/ft_framework/source/functions/assert/assert.h \
+ /home/bhildebr/github/ft_framework/source/functions/deallocate/deallocate.h \
+ /home/bhildebr/github/ft_framework/source/functions/debug/debug.h \
+ /home/bhildebr/github/ft_framework/source/functions/delete/delete.h \
+ /home/bhildebr/github/ft_framework/source/functions/error/error.h \
+ /home/bhildebr/github/ft_framework/source/functions/new/new.h \
+ /home/bhildebr/github/ft_framework/source/functions/print/print.h \
+ /home/bhildebr/github/ft_framework/source/types/character/character.h \
+ /home/bhildebr/github/ft_framework/source/types/file/file.h \
+ /home/bhildebr/github/ft_framework/source/types/string/string.h \
+ /home/bhildebr/github/ft_framework/source/types/vector/vector.h \
+ /home/bhildebr/github/ft_framework/source/types/integer/integer.h \
+ /home/bhildebr/github/ft_framework/source/types/fractional/fractional.h
--- source/lab.c
+++ source/lab.c
@@ -1,20 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   lab.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bhildebr <bhildebr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/15 14:38:11 by bhildebr          #+#    #+#             */
-/*   Updated: 2024/02/15 15:10:26 by bhildebr         ###   ########.fr       */
+/*   Created: 2024/02/21 12:13:29 by bhildebr          #+#    #+#             */
+/*   Updated: 2024/03/01 16:59:27 by bhildebr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "main.h"
-#include <stdio.h>
-
-int	main(void)
+t_i32	main(/* copy from ft_name */)
 {
-	printf("Hello world!\n");
+	framework_setup();
+	push_swap(args);
+	framework_teardown();
+}
+
+int	lab(void)
+{
+	framework_init();
+	framework_destroy();
+	loopinrange(42, 0, just_do_it);
 	return (0);
 }
